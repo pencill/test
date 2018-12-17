d7267d3f122e047eb71dd86d1748f08e3289496f
pencil <254212536@qq.com>
2018-12-17 18:14:12 +0800

    dd

--- index.php
+++ index.php
@@ -28,6 +28,7 @@
 	$str_available_languages = substr($str_available_languages, 0, -3);
 ?>
 
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
